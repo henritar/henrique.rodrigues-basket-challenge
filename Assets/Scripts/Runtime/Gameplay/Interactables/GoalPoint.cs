8c6b1a4e3a4c1c106476aebfee8d0b1802e188aa
henritar <henriquetorres92@gmail.com>
2025-09-13 19:54:52 -0300

    [ball-physics] Update ball position

---
+++ Assets/Scripts/Runtime/Gameplay/Interactables/GoalPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Runtime.Gameplay.Interactables
+{
+    public class GoalPoint : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Managers/ShotManager.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Runtime.Gameplay.Interactables;
+using Assets.Scripts.Runtime.Shared;
+
+namespace Assets.Scripts.Runtime.Managers
+{
+    public class ShotManager : BaseManager
+    {
+        private readonly Ball _ballPresenter;
+
+        public override void Initialize()
+        {
+           _isInitialized = true;
+        }
+    }
+}
